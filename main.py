af1301e8a1b95cc72a4bf8d0451b73ad5c4feeee
oviyaan07 <oviyaansingh04@gmail.com>
2024-11-10 12:13:45 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a=4.71
+if type(a) is int:
+    print("The value is an integer")
+else:
+    print("The value is not an integer")
+x=20
+y=21.65
+if x is y:
+    print("Both are of same data type")
+else:
+    print("both are of different data types")
